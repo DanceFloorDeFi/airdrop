493bda46c761d67940826779b83b3eedd7492e5e
Shegen <84243160+shegenerates@users.noreply.github.com>
2021-08-16 18:18:04 -0400

    combine duplicates

---
+++ removeDuplicates.py
@@ -0,0 +1,32 @@
+import csv
+# open file in read mode
+with open('TheList.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = csv.reader(read_obj)
+    # Iterate over each row in the csv using reader object
+
+    data = {}
+
+    counter = 0
+
+    for row in csv_reader:
+        # row variable is a list that represents a row in csv
+        print(row)
+        if row[0] in data:
+            data[row[0]] = data[row[0]] + float(row[1])
+        else:
+            data[row[0]] = float(row[1])
+
+        counter = counter + 1
+        if(counter % 100 == 0):
+            print("checked " + str(counter))
+
+print("==== done ====")
+print(data)
+
+with open('FinalList.csv', 'w') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+
+    for key, value in data.items():
+       write.writerow([key, value])
